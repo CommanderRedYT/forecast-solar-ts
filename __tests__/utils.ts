f1d39af0ae92860b6fbc509b0122ca82155f543c
CommanderRedYT <ccommanderred@gmail.com>
2024-06-11 15:21:40 +0200

    Add check to utils

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,5 +1,13 @@
 import fs from 'fs';
 import { jsonc } from 'jsonc';
 
-export const loadFixture = (filename: string): string => fs.readFileSync(`__tests__/fixtures/${filename}`, 'utf-8');
+export const loadFixture = (filename: string): string => {
+    const content = fs.readFileSync(`__tests__/fixtures/${filename}`, 'utf-8');
+
+    if (typeof content !== 'string' || content.length < 50) {
+        throw new Error('Invalid file!');
+    }
+
+    return content;
+};
 export const loadFixtureJSON = (filename: string): any => jsonc.parse(loadFixture(filename));
